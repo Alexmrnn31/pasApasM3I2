0c555180313decb3b8fd957baff5f15fec888816
insastrasbge1 <insastrasbge1@gmail.com>
2022-09-25 22:57:27 +0200

    debut V2022 S2

--- src/main/java/fr/insa/beuvron/cours/m3/pasApasM3I2/Test.java
+++ src/main/java/fr/insa/beuvron/cours/m3/pasApasM3I2/Test.java
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with CoursBeuvron.  If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.insa.beuvron.cours.m3.pasApasM3I3;
+package fr.insa.beuvron.cours.m3.pasApasM3I2;
 
 /**
  *
